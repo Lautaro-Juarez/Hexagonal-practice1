091316ffb488c5252e4b1ffa8ebec64603bd0b32
Lautaro Juarez <lautarojuarez232@gmail.com>
2024-06-26 20:29:11 -0300

    uploading new functions in infrastructure and shared folder

---
+++ shared/infrastructure/ServiceContainer.ts
@@ -0,0 +1,20 @@
+import {
+  UserCreate,
+  UserDelete,
+  UserEdit,
+  UserGetAll,
+  UserGetOneById,
+} from "../../User/application/index.barrel";
+import { InMemoryUserRepository } from "../../User/infrastructure/InMemoryUserRepository";
+
+const UserRepository = new InMemoryUserRepository();
+
+export const ServiceContainer = {
+  user: {
+    getAll: new UserGetAll(UserRepository),
+    getOneById: new UserGetOneById(UserRepository),
+    create: new UserCreate(UserRepository),
+    edit: new UserEdit(UserRepository),
+    delete: new UserDelete(UserRepository),
+  },
+};
